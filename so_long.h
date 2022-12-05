0b54c7fb196db77042e8f98473f46267fd5955bb
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-12-06 00:38:42 +0100

    V2

--- so_long.h
+++ so_long.h
@@ -21,10 +21,19 @@
 # include "MLX42/include/MLX42/MLX42.h"
 # include <stdio.h>
 
+//GET_NEXT_LINE
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 char	*ft_get_next_line(int fd);
+size_t	ft_strlen_n(const char *s);
+//CHECK
+char	*ft_strnstr(const char *b, const char *l, size_t len);
+int	    ft_number_columns(int fd);
+int	    ft_number_rows(int fd);
+void	ft_check_walls(char **content, int rows, int columns);
+char	**ft_create_content(int fd, int rows);
+int	    ft_control_char(char **content, int rows, int columns);
 
 #endif
