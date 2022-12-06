232191fc808f15fecf8db82ebd5f9ab148fbabff
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-12-06 19:39:00 +0100

    V5

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 10:30:10 by pespinos          #+#    #+#             */
-/*   Updated: 2022/12/05 18:58:23 by pespinos         ###   ########.fr       */
+/*   Updated: 2022/12/06 19:11:24 by pespinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int	main(int argc, char **argv)
 {
+	mlx_t	*mlx;
 	char	**content;
 	int		rows;
 	int		columns;
 	int		fd;
 	int r = 0;
+	int	width = 0;
+	int height = 0;
 
 	fd = 0;
 	rows = 0;
@@ -29,6 +32,11 @@ int	main(int argc, char **argv)
 		rows = ft_number_rows(fd);
 		fd = open(argv[1], O_RDONLY);
 		columns = ft_number_columns(fd);
+		
+		width = columns * 32;
+		height = rows * 32;
+		mlx = mlx_init(width, height, "PRUEBA SPRITES", false);
+		
 		fd = open(argv[1], O_RDONLY);
 		content = ft_create_content(fd, rows);
 		if (ft_control_char(content, rows, columns))
@@ -37,6 +45,7 @@ int	main(int argc, char **argv)
 				printf("%s\n", content[r++]);
 			ft_check_walls(content, rows, columns);
 		}
+		ft_create_map(content, rows, columns, mlx);
 	}
 	else if (!ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])))
 		write(1, "ERROR\nEXTENSION DE ARCHIVO INVALIDA\n", 36);
