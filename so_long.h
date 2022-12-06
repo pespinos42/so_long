2a4ceff52125be01e1c5f6b09b6879be015a7a80
pespinos42 <113940232+pespinos42@users.noreply.github.com>
2022-12-06 19:39:17 +0100

    V3

--- so_long.h
+++ so_long.h
@@ -6,12 +6,12 @@
 /*   By: pespinos <pespinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 15:46:15 by pespinos          #+#    #+#             */
-/*   Updated: 2022/11/30 17:09:34 by pespinos         ###   ########.fr       */
+/*   Updated: 2022/12/06 17:20:08 by pespinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_UTILS_H
-# define SO_LONG_UTILS_H
+#ifndef SO_LONG_H
+# define SO_LONG_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
@@ -21,19 +21,22 @@
 # include "MLX42/include/MLX42/MLX42.h"
 # include <stdio.h>
 
-//GET_NEXT_LINE
+//GET_NEXT_LINE_UTILS
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
+int		ft_strlen_n(char *str);
+//GET_NEXT_LINE
 char	*ft_get_next_line(int fd);
-size_t	ft_strlen_n(const char *s);
 //CHECK
 char	*ft_strnstr(const char *b, const char *l, size_t len);
-int	    ft_number_columns(int fd);
-int	    ft_number_rows(int fd);
+int		ft_number_columns(int fd);
+int		ft_number_rows(int fd);
 void	ft_check_walls(char **content, int rows, int columns);
 char	**ft_create_content(int fd, int rows);
-int	    ft_control_char(char **content, int rows, int columns);
+int		ft_control_char(char **content, int rows, int columns);
+//DISPLAY
+void	ft_create_map(char **content, int rows, int columns, mlx_t *mlx);
 
 #endif
